f9be6321267bbfbb44014edb84e1529434fc0b6e
Joe <Walktheworld@me.com>
2022-03-14 14:56:11 -0600

    commit tree b8c46eb58f150f5b5c7682c950d460196af34573

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    game.to_json(include: { reviews: { include: :user } })
+  end
+
 end
